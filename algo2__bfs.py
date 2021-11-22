fbfed855f89fd21eb0d36a4cd6cea2bb437aa023
Niv Nagli <itsniv@gmail.com>
2021-11-23 00:16:45 +0200

    bfs : Need a full graph to work with

--- algo2__bfs.py
+++ algo2__bfs.py
@@ -1,6 +1,9 @@
 # Written by : Mor & Tom & Hen & Niv
 
 from queue import Queue
+from Vertex import Vertex
+from Graph import Graph
+from vertex_neighbors import get_neighbours
 
 
 class BFS:
@@ -9,30 +12,98 @@ class BFS:
         self.__graph = g
 
     def run(self, start):
+        """
+        First we will reset the old values of the distances and the pi's fields,
+        afterwards we will run the BFS methodology with the help of the 'Queue' data structure and will be assisted
+        with the 'get_neighbours' method that we receive from the 'vertex_neighbors' file.
+        """
         q = Queue()
-        for vertex in self.__graph.getVertices(): # Reset all the distances first, and the boolean flags.
-            vertex.setDistance(float('inf'))
-            vertex.resetBooleanFlags()
+        self.resetGraph()
 
-        start.setDistance(0)
+        start.set_dv(0)
         q.put(start)
         while not q.empty():
             currentVertex = q.get()
-            for neighbour in currentVertex.getValidNeighbours(): # This function need to return for us only the valid base that we can move to.
-                if neighbour.getDistance() > currentVertex.getDistance() + 1:
-                    neighbour.setDistance(currentVertex.getDistance() + 1)
+            for neighbour in currentVertex.near_lst:
+                keyA, keyB = self.findDiffVec(currentVertex, neighbour)
+                if neighbour.base_a[keyA] or neighbour.base_b[keyB]:
+                    continue
+                neighbour.base_a[keyA] = True
+                neighbour.base_b[keyB] = True
+                if neighbour.get_dv() > currentVertex.get_dv() + 1:
+                    neighbour.set_dv(currentVertex.get_dv() + 1)
                     q.put(neighbour)
-                    neighbour.setParent(currentVertex)
+                    # neighbour.set_pi(currentVertex) TODO: Maybe in the future ...
         return 0
 
+    def resetGraph(self):
+        # Reset all the distances first, and the boolean flags.
+        for vertex in self.__graph.vertex_lst:
+            vertex.set_dv(float('inf'))
+            # vertex.set_pi(0) TODO: Maybe in the future ...
+            self.resetBooleanFlags(vertex)
 
-"""
- TODO:
- Validation of neighbors visitation status requires more work. Implementation pending graph, required methods will be implemented afterwards.
-"""
+    def resetBooleanFlags(self, vertex):
+        for key in vertex.base_a:
+            vertex.base_a[key] = False
+        for key in vertex.base_b:
+            vertex.base_b[key] = False
+
+    def findDiffVec(self, vertex, vertex_Dest):
+        """
+        This method will return
+        """
+        keyA = keyB = 0
+        for key, value in vertex_Dest.base_a.items():
+            if key not in vertex.base_a:
+                keyA = key
+
+        for key, value in vertex_Dest.base_b.items():
+            if key not in vertex.base_b:
+                keyB = key
+
+        return keyA, keyB
+
+    def retAllVertexWithDistance(self, d):
+        """
+        Side method that need to be removed in final stage, we use her to detect bugs.
+        """
+        res = []
+        for vertex in self.__graph.vertex_lst:
+            if vertex.get_dv() == d:
+                res.append(vertex)
+        return res
 
 
+if __name__ == "__main__":
+    baseA__test = {"0001": False, "0010": False, "0100": False, "1000": False}
+    baseB__test = {"1011": False, "1101": False, "1110": False, "1111": False}
+    # baseA__test = {"001": False, "010": False, "100": False}
+    # baseB__test = {"011": False, "101": False, "110": False}
+    # TODO :
+    """
+    המימוש תקין, השינוי של הדגלים מתרחש הסריקה גם כן מתרחשת כמו שצריך, אבל יש בעיה כרגע במבנה נתונים של הגרף
+    שהוא לא מייצר בכלל את כל הקודקודים ראו דוג׳ ששמנו פה לשני הדרכים לייצור הגרף
+    בבסיס 4 אנחנו מקבלים בכלל גרף עם 2 קודקודים כולל הבסיס שהוכנס, ובדוג׳ השניה אנחנו מקבלים 5 קודקודים,
+    כמו כן עבור בסיסים עם וקטורים זהים הבניה של הגרף נכשלת,
+    הסריקה שבנינו צריכה לעבוד עם גרף מלא כפי שרן תיאר במצב הנוכחי שהוא גם כנראה לא תקין היא לא תעבוד.
+    """
 
+    # TODO: שימו לב שבדוג׳ הנוכחית יש לנו רק 2 קודקודים בגרף לכן הסריקה לא תעבוד
+    graph__test = Graph(baseA__test, baseB__test, 4)
+    print("We have only 2 vertex here read the print that come afterward:\n")
+    graph__test.print_graph()
+    BFS__test = BFS(graph__test)
+    BFS__test.run(graph__test.vertex_lst[0])
+    x = BFS__test.retAllVertexWithDistance(3)
+    y = 2
 
-
-
+    """
+    דוג׳ לקריסה של הבנאי
+    """
+    badBuild_baseA = {"10": False, "01": False}
+    badBuild_baseB = {"10": False, "11": False}
+    try:
+        graphCollapse = Graph(badBuild_baseA, badBuild_baseB, 2)
+    except:
+        print("Need To Be Fixed.")
